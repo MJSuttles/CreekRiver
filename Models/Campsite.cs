9c808e0e18eb7a8f6dcf92113da8d1da92fa26e1
MJSuttles <rolltiderolldad@gmail.com>
2025-02-17 21:07:16 -0600

    initial commit

---
+++ Models/Campsite.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CreekRiver.Models;
+
+public class CampsiteType
+{
+  public int Id { get; set; }
+  [Required]
+  public string Nickname { get; set; }
+  public string ImageUrl { get; set; }
+  public int CampsiteTypeId { get; set; }
+  public CampsiteType CampsiteType { get; set; }
+  public List<Reservation> Reservations { get; set; }
+}
